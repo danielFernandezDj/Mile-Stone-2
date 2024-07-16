91134064f4e2dbeb8b1a154f27c255843ece68b6
oviedo2522 <oviedo2522@gmail.com>
2024-07-15 21:26:47 -0700

    New NavBar

--- README.md
+++ README.md
@@ -68,3 +68,8 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+import \* as React from 'react';
+import { AppBar, Box, Container, IconButton, Typography } from "@mui/material";
+import { Menu, MenuIcon, Toolbar, Avatar } from "@mui/material";
+import { Button, Tooltip, MenuItem, AdbIcon } from "@mui/material";
--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 // MUI
 import * as React from 'react';
-import { Container, Box } from '@mui/material';
+import { Container, Box, dividerClasses } from '@mui/material';
 
 // Components
 import TireCard from './components/TireCard';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <Container sx={{
-      border: '4px solid #b4e4fc',
-      height: '100vh',
-    }}>
-      <TireCard />
-    </Container>
+    <div>
+      <NavBar />
+      <Container sx={{
+        border: '4px solid #b4e4fc',
+        height: '100vh',
+      }}>
+        <TireCard />
+      </Container>
+    </div>
   );
 }
 
