c18a60e93e2c8d6eb771137a071802b896f10be0
Daniel Fernandez <mdanif9728@gmail.com>
2024-07-15 18:16:44 -0700

    creating the first card component stucture

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+// MUI
+import * as React from 'react';
+import { Container, Box } from '@mui/material';
 
+// Components
+import TireCard from './components/TireCard';
 
 function App() {
   return (
-    <main>
-      
-    </main>
+    <Container sx={{
+      border: '4px solid #b4e4fc',
+      height: '100vh',
+    }}>
+      <TireCard />
+    </Container>
   );
 }
 
--- src/components/TireCard.js
+++ src/components/TireCard.js
@@ -1,1 +1,63 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
+// MUI dependencies
+import * as React from 'react';
+import { Card, CardActions, CardContent, CardMedia } from '@mui/material';
+import { ThemeProvider, createTheme, Stack, Box } from '@mui/material';
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
+import { Typography, Button } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    text: {
+      primary: '#173A5E',
+      secondary: '#46505A',
+    },
+    action: {
+      active: '#001E3C',
+    },
+    addButton: {
+      main: '#2386cf',
+      light: '#84d3fa',
+      dark: '#28a7f2',
+      contrastText: '#242105',
+    },
+  }
+});
+
+export default function TireCard() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Card sx={{ maxWidth: 345, mt: 2, mb: 2 }} >
+        <Stack direction={'row'} >
+          <CardMedia
+            component="img"
+            image="/images/Designer2.png"
+            title="Tire"
+            sx={{ width: '50%', margin: 'auto', }}
+          />
+
+          <CardContent
+            width={'50%'}
+            spacing={3}
+            direction="column"
+          >
+            <Typography variant='h5' fontWeight="700"> Good Year A/T </Typography>
+            <Typography variant='h6' fontWeight="700"> LT225/75.R16  </Typography>
+            <Typography variant='h4' fontWeight="700" > $125 </Typography>
+
+            <CardActions>
+              <Button variant="contained" color={"addButton"} sx={{ width: '130px', }}>
+                <AddRoundedIcon fontSize='large' sx={{ color: 'white', }} />
+                <Typography variant='h5' fontWeight="700" textTransform={"none"} color={'white'}> Add </Typography>
+              </Button>
+            </CardActions>
+          </CardContent>
+        </Stack>
+      </Card>
+    </ThemeProvider>
+  );
+}
