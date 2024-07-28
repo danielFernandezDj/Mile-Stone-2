c93f7de098d43ba4beeedda9511ce9d99450d71e
Daniel Fernandez <mdanif9728@gmail.com>
2024-07-28 16:56:42 -0700

    finish with styling the Input

--- src/components/Input.js
+++ src/components/Input.js
@@ -8,6 +8,8 @@ export default function NewInput() {
   const [tread, setTread] = useState("");
   const apiUrl = 'http://localhost:4000/api/tires';
 
+  const inputMargin = 4;
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -25,73 +27,86 @@ export default function NewInput() {
     }
   };
 
-  const ariaLabel = { 'aria-label': 'description' };
-
   return (
     <>
       <Box
         textAlign={"center"}
       >
-        <Typography variant="h1" fontWeight={"bold"}>Tire List </Typography>
+        <Typography variant="h1" fontWeight={"bold"}>Tire List</Typography>
       </Box>
 
       <form onSubmit={handleSubmit}>
-        <InputLabel htmlFor="brand_name">Brand Name</InputLabel>
-        <Input sx={{ marginRight: 5, }}
-          required
-          id="brand_name"
-          placeholder="Continental"
-          inputProps={{
-            'aria-label': 'Brand name',
-            minLength: 2,
-            maxLength: 35,
-          }}
-          type="text"
-          value={brand}
-          onChange={(e) => setBrand(e.target.value)}
-        />
+        <Box sx={{
+          display: 'flex', m: 4,
+          direction: 'row',
+          justifyContent: 'center',
+          height: 45,
+        }}
+        >
+          <Box>
+            <InputLabel htmlFor="brand_name" sx={{ fontSize: 12.5, }}>Brand Name</InputLabel>
+            <Input sx={{ marginRight: inputMargin, }}
+              required
+              id="brand_name"
+              placeholder="Continental"
+              inputProps={{
+                'aria-label': 'Brand name',
+                minLength: 2,
+                maxLength: 35,
+              }}
+              type="text"
+              value={brand}
+              onChange={(e) => setBrand(e.target.value)}
+            />
+          </Box>
 
-        <InputLabel htmlFor="tire-size">Tire Size</InputLabel>
-        <Input sx={{ marginRight: 5, }}
-          required
-          id="tire-size"
-          placeholder="225/70/15"
-          inputProps={{
-            'aria-label': 'Tire size',
-            minLength: 8,
-            maxLength: 10,
-          }}
-          type="text"
-          value={size}
-          onChange={(e) => setSize(e.target.value)}
-        />
 
-        <Button
-          variant="contained"
-          size="small"
-          type="submit"
-          disabled={
-            !brand.trim() &&
-            !size.trim() &&
-            !tread.trim()
-          }>
-          Add
-        </Button>
+          <Box>
+            <InputLabel htmlFor="tire-size" sx={{ fontSize: 12.5, }}>Tire Size</InputLabel>
+            <Input sx={{ marginRight: inputMargin, }}
+              required
+              id="tire-size"
+              placeholder="225/70/15"
+              inputProps={{
+                'aria-label': 'Tire size',
+                minLength: 8,
+                maxLength: 10,
+              }}
+              type="text"
+              value={size}
+              onChange={(e) => setSize(e.target.value)}
+            />
+          </Box>
 
-        <InputLabel htmlFor="tread_pattern">Tread Pattern</InputLabel>
-        <Input sx={{ marginRight: 5, }}
-          required
-          id="tread_pattern"
-          placeholder="All Season"
-          inputProps={{
-            'aria-label': 'Tread Pattern',
-            minLength: 2,
-            maxLength: 20,
-          }}
-          type="text"
-          value={tread}
-          onChange={(e) => setTread(e.target.value)}
-        />
+          <Box>
+            <InputLabel htmlFor="tread_pattern" sx={{ fontSize: 12.5, }}>Tread Pattern</InputLabel>
+            <Input sx={{ marginRight: inputMargin, }}
+              required
+              id="tread_pattern"
+              placeholder="All Season"
+              inputProps={{
+                'aria-label': 'Tread Pattern',
+                minLength: 2,
+                maxLength: 20,
+              }}
+              type="text"
+              value={tread}
+              onChange={(e) => setTread(e.target.value)}
+            />
+          </Box>
+
+          <Button
+            variant="contained"
+            size="small"
+            type="submit"
+            disabled={
+              !brand.trim() &&
+              !size.trim() &&
+              !tread.trim()
+            }>
+            Add
+          </Button>
+        </Box>
       </form >
     </>
   );
